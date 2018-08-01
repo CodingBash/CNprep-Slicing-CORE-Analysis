d39f1f123db6d7f94eedf897f95bd71aaae6f0c2
CodingBash <bbecerr@outlook.com>
2018-08-01 15:15:19 -0400

    Reselected COREs and compared CORE profiles
    -Reselected COREs for prev_run1 and prev_run_7_28_2018_1 including the
    CORE score as the 4th column of the BED file
    
    -Created script to visualize the CORE with the region on X axis and the
    CORE score as the Y axis. Input implementations are in different
    files/directories

--- analysis/selectCores.R
+++ analysis/selectCores.R
@@ -14,9 +14,12 @@ source("scripts/helperFunctions.R")
 #
 p_threshold <- 0.002
 
-rdsFile <- "./output/coresResults/ADnewCOREobjBP_slicing2.rds"
-tableFile <- "./output/coresResults/ADcoreTableBP_slicing2.csv"
-outputBed <- "./output/selectedCores/ADcoresBP.bed"
+event <- "AD"
+output <- "output/coresResults"
+dir <- "prev_run_7_28_2018_x_1"
+rdsFile <- paste0("./", output, "/", dir, "/", event, "newCOREobjBP_slicing2.rds")
+tableFile <- paste0("./", output, "/", dir, "/", event, "coreTableBP_slicing2.csv")
+outputBed <- paste0("./", output, "/", dir, "/selectedCores/", event, "selectedCoresBP.bed")
 
 #
 # Get core information
@@ -28,5 +31,5 @@ coreTable <- read.table(tableFile, header = TRUE, sep = ",") # Retrieve CORE tab
 # Subset core information using p_threshold then write to file
 #
 coreTable <- coreTable[which(obj$p<p_threshold),] # Filter cores based on p-value threshold
-coreTable <- coreTable[,c(2,3,4)] # Now convert to BED format
+coreTable <- coreTable[,c(2,3,4,5)] # Now convert to BED format
 write.table(coreTable, sep = "\t", quote = FALSE, col.names = FALSE, row.names = FALSE, file = outputBed) # Write to bed file
---
+++ analysis/visualizeCoreComparison.R
@@ -0,0 +1,42 @@
+#
+# Compare COREs and their scores across different implementations
+#
+
+setwd("~/Git-Projects/Git-Research-Projects/CNprep-Slicing-CORE-Analysis/")
+source("analysis/genomicFeatureAssignment.R") # For retrieval of COREs
+
+chromosomeSizes <- readRDS("./resources/chromosomeSizes.rds")
+
+event <- "AD"
+output <- "output/coresResults"
+dirs <- c("prev_run1_1", "prev_run_7_28_2018_x_1")
+
+x0 <- c()
+x1 <- c()
+y <- c()
+
+for(dir.i in seq_along(dirs)){
+  outputBed <- paste0("./", output, "/", dirs[dir.i], "/selectedCores/", event, "selectedCoresBP.bed") # CORES must be same file name
+  cores <- retrieveCores(outputBed) # BED file of amplification recurrent regions  
+  cores <- chromsomeToAbsoluteBPConversion(cores, chromosomeSizes)
+  coresStart <- cores[[2]]
+  coresEnd <- cores[[3]]
+  coresScore <- cores[[4]]
+  x0[[dir.i]] <- coresStart
+  x1[[dir.i]] <- coresEnd
+  y[[dir.i]] <- coresScore
+}
+
+
+plot(range(c(x0, x1)), range(y), type="n")
+for(i in seq_along(dirs)){
+  segments(x0=x0[[i]], x1=x1[[i]], y0=y[[i]], y1=y[[i]], col = i, lwd = 4)
+}
+legend("topleft", legend=dirs, col = seq_along(dirs), lty=1, cex=0.8)
+
+for(chrom.i in seq(nrow(chromosomeSizes))){
+  abline(v=sum(sapply(seq(chrom.i), function(chrom.j){
+    return(chromosomeSizes[chrom.j, ]$size)
+  })))
+}
+
